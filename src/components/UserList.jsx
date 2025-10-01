a493c8463ba7d3009c5a41a4164706ebc5df0611
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-01 18:05:05 +0500

    creating redux store and add get request for fetching data using async thunks

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { fetchUsers } from "../Store";
 
 function UserList() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers())
+    }, [dispatch]);
+
   return (
     <div>
-      Users List
+        user list
     </div>
   )
 }
