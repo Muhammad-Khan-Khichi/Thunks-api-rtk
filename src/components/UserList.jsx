6b75b32b92109d286f9529173736b5c620a535d8
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-01 18:40:18 +0500

    adding animation in project using lottie files

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,20 +1,36 @@
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { fetchUsers } from "../Store";
+import Lottie from "lottie-react";
+import Loader from "../Loader.json";
+import errorLoader from "../error.json"
 
 function UserList() {
 
     const dispatch = useDispatch();
+    const {isLoading, data, error} = useSelector((state) => {
+        return state.users
+})
 
     useEffect(() => {
         dispatch(fetchUsers())
     }, [dispatch]);
 
-  return (
-    <div>
-        user list
+    if(isLoading){
+        return <div>
+            <Lottie loop={true} animationData={Loader}/>
+        </div>
+    }
+
+    if (error) {
+        return <div className="w-fit h-screen">
+            <Lottie className="h-full flex items-center justify-center" loop={true} animationData={errorLoader}/>
+        </div>
+    }
+
+    return <div>
+        {data.length}
     </div>
-  )
 }
 
 export default UserList
