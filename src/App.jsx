9d84984787c5049d251b4db23a6837bab7bf84ab
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-01 17:29:13 +0500

    creating the initial files of redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import UserList from './components/UserList'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <UserList/>
+    </div>
   )
 }
 
---
+++ src/components/UserList.jsx
@@ -0,0 +1,10 @@
+
+function UserList() {
+  return (
+    <div>
+      Users List
+    </div>
+  )
+}
+
+export default UserList
