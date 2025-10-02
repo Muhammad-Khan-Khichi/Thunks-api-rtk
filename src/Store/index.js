21059b6ee3c796465d3f560328be13f70285966e
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-10-02 17:31:23 +0500

    adding the album list

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,16 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./Slices/userSlice";
+import { albumApi } from "./apis/albumApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 
 
 const store = configureStore({
     reducer: {
         users: userReducer,
+        [albumApi.reducerPath]: albumApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(albumApi.middleware)
     }
-})
+});
+
+setupListeners(store.dispatch)
 
 export default store
 
 export * from './thunks/fetchUsers'
 export * from './thunks/addUsers'
 export * from './thunks/removeUser'
+export {useFetchAlbumQuery} from './apis/albumApi'
--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import Loader from "../Loader.json";
+import { useFetchAlbumQuery } from '../Store'
+import ExpandablePannel from "./ExpandablePannel";
 
-function AlbumList({user}) {
+function AlbumList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumQuery(user)
+  
+  let content;
+  if (isLoading) {
+     <div>Loading...</div>
+  }else{
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+      return <ExpandablePannel key={album.id} header={header}>
+        List of photos in the album
+      </ExpandablePannel>
+    })
+  }
   return (
-    <div>
-      Album for {user.name}
-    </div>
+    <>
+      <div>
+         Album for {user.name}
+      </div>
+      <div>{content}</div>
+    </>
   )
 }
 
